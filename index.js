688130853360ce7ae0b983c2b38ddfe88f96ba93
robertelle11 <145513996+robertelle11@users.noreply.github.com>
2023-10-24 18:04:36 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+function showImage1() {
+  var img = document.getElementById("img1");
+  var x = document.getElementById("display1");
+  img.style.visibility = "visible";
+  x.style.visibility = "visible";
+}
+
+function hideImage1() {
+  var hide = document.getElementById("img1");
+  var y = document.getElementById("display1");
+  hide.style.visibility = "hidden";
+  y.style.visibility = "hidden";
+}
+
+//image 2
+function showImage2() {
+  var img = document.getElementById("img2");
+  var x = document.getElementById("display2");
+  img.style.visibility = "visible";
+  x.style.visibility = "visible";
+}
+
+function hideImage2() {
+  var hide = document.getElementById("img2");
+  var y = document.getElementById("display2");
+  hide.style.visibility = "hidden";
+  y.style.visibility = "hidden";
+}
+
+//image 3
+function showImage3() {
+  var img = document.getElementById("img3");
+  var x = document.getElementById("display3");
+  img.style.visibility = "visible";
+  x.style.visibility = "visible";
+}
+
+function hideImage3() {
+  var hide = document.getElementById("img3");
+  var y = document.getElementById("display3");
+  hide.style.visibility = "hidden";
+  y.style.visibility = "hidden";
+}
